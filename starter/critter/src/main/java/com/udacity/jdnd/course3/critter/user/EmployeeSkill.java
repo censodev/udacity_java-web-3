09459145f980e7c9eb0e8509a249197e5fa48950
Phuong Bui <buiphuongaptutc@gmail.com>
2022-11-25 10:28:49 +0700

    datasource cfg

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeSkill.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeSkill.java
@@ -1,11 +1,8 @@
 package com.udacity.jdnd.course3.critter.user;
 
-import javax.persistence.Embeddable;
-
 /**
  * A example list of employee skills that could be included on an employee or a schedule request.
  */
-@Embeddable
 public enum EmployeeSkill {
     PETTING, WALKING, FEEDING, MEDICATING, SHAVING;
 }
